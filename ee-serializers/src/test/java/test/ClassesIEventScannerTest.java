9e591b78b33b5c4968eaee7553d420c9280d1ace
Zh G <perkoz@mail.bg>
2018-01-27 22:26:08 +0200

    Added ClassesIEventScanner tests

---
+++ ee-serializers/src/test/java/test/ClassesIEventScannerTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import net.uniplovdiv.fmi.cs.vrs.event.*;
+import net.uniplovdiv.fmi.cs.vrs.event.serializers.engine.ClassesIEventScanner;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ClassesIEventScannerTest {
+
+    private HashSet<Class<? extends IEvent>> foundEventClasses = new HashSet<Class<? extends IEvent>>() {
+        {
+            add(Event.class);
+            add(DomainEvent.class);
+            add(SystemEvent.class);
+            add(EmergencyEvent.class);
+        }
+    };
+
+    @Test
+    void defaultScanParams() {
+        ClassesIEventScanner cis = new ClassesIEventScanner();
+        assertTrue(cis.getFoundEventClasses().isEmpty());
+        assertArrayEquals(new String[] { IEvent.class.getPackage().getName() }, cis.getPackagesToScan());
+        assertEquals(foundEventClasses, cis.scan());
+        assertEquals(foundEventClasses, cis.getFoundEventClasses());
+    }
+
+    @Test
+    void customScanParams() {
+        ClassesIEventScanner cis = new ClassesIEventScanner(IEvent.class.getPackage().getName());
+        assertTrue(cis.getFoundEventClasses().isEmpty());
+        assertArrayEquals(new String[] { IEvent.class.getPackage().getName(), Event.class.getPackage().getName() },
+            cis.getPackagesToScan());
+        assertEquals(foundEventClasses, cis.scan());
+        assertEquals(foundEventClasses, cis.getFoundEventClasses());
+    }
+}
