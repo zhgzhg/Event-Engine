5afe3be50c0f58306c1ec87b48ba7b310fe51b8d
Zh G <perkoz@mail.bg>
2018-05-01 22:35:08 +0300

    Experimenting with classpath and new surefire plugin results

--- ee-serializers/src/test/java/test/ClassesIEventScannerTest.java
+++ ee-serializers/src/test/java/test/ClassesIEventScannerTest.java
@@ -2,8 +2,11 @@ package test;
 
 import net.uniplovdiv.fmi.cs.vrs.event.*;
 import net.uniplovdiv.fmi.cs.vrs.event.serializers.engine.ClassesIEventScanner;
+//import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+//import java.net.URL;
+//import java.util.ArrayList;
 import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -21,6 +24,43 @@ public class ClassesIEventScannerTest {
             add(EmergencyEvent.class);
         }
     };
+
+    /*private static String whereFrom(Object o) {
+        if ( o == null ) {
+            return null;
+        }
+        Class<?> c = (o instanceof Class ? (Class<?>) o : o.getClass());
+        ClassLoader loader = c.getClassLoader();
+        if ( loader == null ) {
+            // Try the bootstrap classloader - obtained from the ultimate parent of the System Class Loader.
+            loader = ClassLoader.getSystemClassLoader();
+            while ( loader != null && loader.getParent() != null ) {
+                loader = loader.getParent();
+            }
+        }
+        if (loader != null) {
+            String name = c.getCanonicalName();
+            URL resource = loader.getResource(name.replace(".", "/") + ".class");
+            if ( resource != null ) {
+                return resource.toString().replace("/" + c.getSimpleName() + ".class", "")
+                        .replaceFirst("^(\\w+:\\/{1,2})", "");
+
+            }
+        }
+        return "Unknown";
+    }
+
+    private ArrayList<String> ieventClassPathStr;
+
+    @BeforeEach
+    void determineIEventClassPath() {
+        String _ieventClassPathStr = whereFrom(IEvent.class);
+        System.out.println(_ieventClassPathStr);
+        if (_ieventClassPathStr != null) {
+            this.ieventClassPathStr = new ArrayList<>(1);
+            this.ieventClassPathStr.add(_ieventClassPathStr);
+        }
+    }*/
 
     @Test
     void defaultScanParams() {
