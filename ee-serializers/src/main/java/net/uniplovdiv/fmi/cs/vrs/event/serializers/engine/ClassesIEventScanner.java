4ef6fb58019617675d7669655f8f9c47ec335f98
Zh G <perkoz@mail.bg>
2018-05-01 22:36:19 +0300

    Workarounds for classpath scanning under Java 9 modules and later

--- ee-serializers/src/main/java/net/uniplovdiv/fmi/cs/vrs/event/serializers/engine/ClassesIEventScanner.java
+++ ee-serializers/src/main/java/net/uniplovdiv/fmi/cs/vrs/event/serializers/engine/ClassesIEventScanner.java
@@ -4,7 +4,8 @@ import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 import net.uniplovdiv.fmi.cs.vrs.event.*;
 import net.uniplovdiv.fmi.cs.vrs.event.IEvent;
 
-import java.util.HashSet;
+import java.io.File;
+import java.util.*;
 
 /**
  * Retriever of the classes implementing {@link IEvent} interface.
@@ -27,8 +28,8 @@ public class ClassesIEventScanner {
     }
 
     /**
-     * Constructor specifying the packages to be scanned for classes implementing {@link IEvent} interface plus
-     * {@link net.uniplovdiv.fmi.cs.vrs.event} package.
+     * Constructor specifying the packages to be scanned for classes implementing {@link IEvent} interface to which
+     * {@link net.uniplovdiv.fmi.cs.vrs.event} package gets appended too.
      * @param packages The Java packages to be scanned. If is set to null or empty array only the default package will
      *                 be scanned.
      */
@@ -47,6 +48,87 @@ public class ClassesIEventScanner {
     }
 
     /**
+     * Constructor specifying classes to be included as a mandatory part of the scanning, as well as the packages to be
+     * scanned for classes implementing {@link IEvent} interface to which {@link net.uniplovdiv.fmi.cs.vrs.event}
+     * package gets appended too.
+     * @param classes The list manually specified of classes implementing {@link IEvent} interface. If is set null or is
+     *                empty the parameter gets ignored.
+     * @param packages The Java packages to be scanned. If is set to null or empty array only the default package will
+     *                 be scanned.
+     */
+    public ClassesIEventScanner(Collection<Class<? extends IEvent>> classes, String... packages) {
+        foundEventClasses = new HashSet<>();
+        int packagesLength = 1 + (packages != null ? packages.length : 0);
+        packagesToScan = new String[packagesLength];
+        packagesToScan[0] = IEvent.class.getPackage().getName();
+        if (packages != null) {
+            for (int i = 0, j = 1; i < packages.length; ++i, ++j) {
+                packagesToScan[j] = packages[i];
+            }
+        }
+        fastClasspathScanner = new FastClasspathScanner(packagesToScan)
+                .matchClassesImplementing(IEvent.class, matchedClass -> foundEventClasses.add(matchedClass));
+        if (classes != null && !classes.isEmpty()) {
+            classes.forEach(c -> { if (c != null) this.foundEventClasses.add(c); } );
+        }
+    }
+
+    /**
+     * A temporary workaround for the inability of the scanner in some cases to retrieve some classes.
+     * @param r Executed if the runtime version is 9 or later.
+     */
+    @Deprecated
+    private void actIfJava9OrLater(Runnable r) {
+        try {
+            Runtime.Version ver = Runtime.Version.parse(System.getProperty("java.version"));
+            if (ver.major() >= 9 && r != null) {
+                r.run();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    /* Not tested well enough
+     * Constructor specifying the class paths from which the classes to be scanned and the packages to be scanned for
+     * classes implementing {@link IEvent} interface plus {@link net.uniplovdiv.fmi.cs.vrs.event} package.
+     * @param classPathElements The class paths to be scanned. If null the default classpath will be used. For every of
+     *                          the provided elements method toString() will be used to determine the actual path.
+     * @param packages The Java packages to be scanned. If is set to null or empty array only the default package will
+     *                 be scanned.
+     *
+    public ClassesIEventScanner(Iterable<?> classPathElements, String... packages) {
+        foundEventClasses = new HashSet<>();
+        int packagesLength = 1 + (packages != null ? packages.length : 0);
+        packagesToScan = new String[packagesLength];
+        packagesToScan[0] = IEvent.class.getPackage().getName();
+        if (packages != null) {
+            for (int i = 0, j = 1; i < packages.length; ++i, ++j) {
+                packagesToScan[j] = packages[i];
+            }
+        }
+
+        FastClasspathScanner _fastClasspathScanner = new FastClasspathScanner(packagesToScan).verbose(true);
+        if (classPathElements != null) {
+            String classpath = "";
+            try {
+                classpath = System.getProperty("java.class.path");
+                if (classpath == null) classpath = "";
+            } catch (Exception ex) {
+                classpath = "";
+                ex.printStackTrace(System.err);
+            }
+            String[] classpathEntries = classpath.split(File.pathSeparator);
+            final HashSet<String> elements = new HashSet<>(Arrays.asList(classpathEntries));
+            classPathElements.forEach(c -> elements.add(c.toString()));
+
+            _fastClasspathScanner = _fastClasspathScanner.overrideClasspath(elements);
+        }
+        fastClasspathScanner = _fastClasspathScanner
+                .matchClassesImplementing(IEvent.class, matchedClass -> foundEventClasses.add(matchedClass));
+    }*/
+
+    /**
      * Returns an array of the packages that will be scanned for classes implementing {@link IEvent} interface.
      * @return Array of the packages that will be scanned.
      */
@@ -61,6 +143,12 @@ public class ClassesIEventScanner {
     public HashSet<Class<? extends IEvent>> scan() {
         this.foundEventClasses.clear();
         fastClasspathScanner.scan();
+        this.actIfJava9OrLater(() -> {
+            foundEventClasses.add(Event.class);
+            foundEventClasses.add(SystemEvent.class);
+            foundEventClasses.add(DomainEvent.class);
+            foundEventClasses.add(EmergencyEvent.class);
+        });
         return this.foundEventClasses;
     }
 
