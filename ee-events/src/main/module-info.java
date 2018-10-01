1edb3f42200043bf891244ede2627602b6e4ac38
Zh G <perkoz@mail.bg>
2018-10-02 00:59:12 +0300

    Open net.uniplovdiv.fmi.cs.vrs.event and its sub-packages to reflection. Open test packages to gson during unit testing.

--- ee-events/src/main/module-info.java
+++ ee-events/src/main/module-info.java
@@ -12,5 +12,9 @@ module net.uniplovdiv.fmi.cs.vrs.event {
     exports net.uniplovdiv.fmi.cs.vrs.event.location;
     exports net.uniplovdiv.fmi.cs.vrs.event.parameters;
     exports net.uniplovdiv.fmi.cs.vrs.event.parameters.comparison;
-    opens net.uniplovdiv.fmi.cs.vrs.event to net.uniplovdiv.fmi.cs.vrs.event.annotations;
+
+    opens net.uniplovdiv.fmi.cs.vrs.event;
+    opens net.uniplovdiv.fmi.cs.vrs.event.location;
+    opens net.uniplovdiv.fmi.cs.vrs.event.parameters;
+    opens net.uniplovdiv.fmi.cs.vrs.event.parameters.comparison;
 }
